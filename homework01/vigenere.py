98081732e1381b183d6c30b2862cb7c9b0c7f1d4
ilyafalkon <sokolov2001ilya@gmail.com>
2020-10-16 02:16:27 +0300

    изменения с помощью black

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,28 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    char_possible_indexes = list(range(ord("a"), ord("z") + 1))
+    char_possible_indexes.extend(range(ord("a"), ord("z") + 1))
+    char_possible_indexes.extend(range(ord("A"), ord("Z") + 1))
+    char_possible_indexes.extend(range(ord("A"), ord("Z") + 1))
+    char_indexes = list(range(ord("A"), ord("Z") + 1))
+    char_indexes.extend(range(ord("a"), ord("z") + 1))
+    shift_for_char = list(range(26))
+    shift_for_char.extend(range(26))
+    shift = dict(zip(char_indexes, shift_for_char))
+    keyword += "."
+    i = 0
+    for char in plaintext:
+        if keyword[i] == ".":
+            i = 0
+        char_index = ord(char)
+        if char_index in shift:
+            char_index = char_possible_indexes[
+                char_possible_indexes.index(char_index) + shift[ord(keyword[i])]
+            ]
+        char = chr(char_index)
+        ciphertext += char
+        i = i + 1
     return ciphertext
 
 
@@ -26,5 +47,27 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    char_possible_indexes = list(range(ord("a"), ord("z") + 1))
+    char_possible_indexes.extend(range(ord("a"), ord("z") + 1))
+    char_possible_indexes.extend(range(ord("A"), ord("Z") + 1))
+    char_possible_indexes.extend(range(ord("A"), ord("Z") + 1))
+    char_possible_indexes.reverse()
+    char_indexes = list(range(ord("A"), ord("Z") + 1))
+    char_indexes.extend(range(ord("a"), ord("z") + 1))
+    shift_for_char = list(range(26))
+    shift_for_char.extend(range(26))
+    shift = dict(zip(char_indexes, shift_for_char))
+    keyword += "."
+    i = 0
+    for char in ciphertext:
+        if keyword[i] == ".":
+            i = 0
+        char_index = ord(char)
+        if char_index in shift:
+            char_index = char_possible_indexes[
+                char_possible_indexes.index(char_index) + shift[ord(keyword[i])]
+            ]
+        char = chr(char_index)
+        plaintext += char
+        i = i + 1
     return plaintext
