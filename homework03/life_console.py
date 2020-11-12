9cae9b87645798654837e246d53c5394b7d0dcb3
ilyafalkon <sokolov2001ilya@gmail.com>
2020-11-12 22:55:08 +0300

    Реализация life_console.py

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -10,13 +10,33 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        for row in range(self.life.rows + 2):
+            for col in range(self.life.cols + 2):
+                if row == 0 or row == self.life.rows + 1:
+                    if col == 0 or col == self.life.cols + 1:
+                        screen.addstr(row, col, "+")
+                    else:
+                        screen.addstr(row, col, "-")
+                else:
+                    if col == 0 or col == self.life.cols + 1:
+                        screen.addstr(row, col, "|")
+                    else:
+                        screen.addstr(row, col, " ")
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for row in range(self.life.rows):
+            for col in range(self.life.cols):
+                if self.life.curr_generation[row][col] == 1:
+                    screen.addstr(row + 1, col + 1, "*")
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        curses.wrapper(self.draw_borders)
+        while self.life.is_changing and not self.life.is_max_generations_exceeded:
+            screen.refresh()
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            self.life.step()
+            screen.getch()
         curses.endwin()
