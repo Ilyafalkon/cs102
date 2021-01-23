3da6dfadfa0814f7c24f95360272045799538dc1
ilyafalkon <sokolov2001ilya@gmail.com>
2021-01-23 13:07:48 +0300

    some fixes

--- exam-winter/map.py
+++ exam-winter/map.py
@@ -1,14 +1,14 @@
 import argparse
 import pathlib
+import typing
 
 
-
-
-def read_map(path: str):
+def read_map(path: pathlib.Path):
     path = pathlib.Path(path)
     with path.open() as f:
         map = f.read()
     return map + "\n"
+
 
 def create_list_with_numbers(map: str):
     grid = []
@@ -23,9 +23,10 @@ def create_list_with_numbers(map: str):
         elif symbol == "☼":
             newline += "5"
         else:
-            grid.append(newline)    
+            grid.append(newline)
             newline = ""
     return grid
+
 
 def make_matrix(grid: list):
     matrix = []
@@ -34,31 +35,50 @@ def make_matrix(grid: list):
         for symbol in line:
             sub_matrix.extend(symbol)
         matrix.append(sub_matrix)
-        sub_matrix = []    
+        sub_matrix = []
     return matrix
+
 
 def find_position(grid: list):
     for row in grid:
         for symbol in row:
             if symbol == "1":
                 return (grid.index(row), row.index(symbol))
+
+
 def find_destination(grid: list):
     for row in grid:
         for symbol in row:
             if symbol == "5":
                 return (grid.index(row), row.index(symbol))
 
-def solve(grid: list, curr_pos: (int, int)):
+
+def solve(grid: list, curr_pos: typing.Tuple[int, int]):
     solved = False
     row2, col2 = find_destination(grid)
     row, col = curr_pos
     print(curr_pos)
-    if grid[row2 - 1][col2] == "1" or grid[row2 + 1][col2] == "1" or grid[row2][col2 - 1] == "1" or grid[row2][col2 + 1] == "1":
+    if (
+        grid[row2 - 1][col2] == "1"
+        or grid[row2 + 1][col2] == "1"
+        or grid[row2][col2 - 1] == "1"
+        or grid[row2][col2 + 1] == "1"
+    ):
         return grid
-    if grid[row + 1][col] != "." and grid[row - 1][col] != "." and grid[row][col + 1] != "." and grid[row][col - 1] != ".":
+    if (
+        grid[row + 1][col] != "."
+        and grid[row - 1][col] != "."
+        and grid[row][col + 1] != "."
+        and grid[row][col - 1] != "."
+    ):
         return None
-    while not solved:    
-        if grid[row2 - 1][col2] == "1" or grid[row2 + 1][col2] == "1" or grid[row2][col2 - 1] == "1" or grid[row2][col2 + 1] == "1":
+    while not solved:
+        if (
+            grid[row2 - 1][col2] == "1"
+            or grid[row2 + 1][col2] == "1"
+            or grid[row2][col2 - 1] == "1"
+            or grid[row2][col2 + 1] == "1"
+        ):
             solved = True
         if grid[row + 1][col] == ".":
             grid[row + 1][col] = "1"
@@ -83,7 +103,7 @@ def solve(grid: list, curr_pos: (int, int)):
             if solve(grid, (row, col + 1)):
                 return solve(grid, (row, col + 1))
             else:
-                grid[row][col + 1] = "3"   
+                grid[row][col + 1] = "3"
     return grid
 
 
@@ -105,11 +125,10 @@ def print_map(matrix: list):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('map')
+    parser.add_argument("map")
     args = parser.parse_args()
     map = read_map(args.map)
     grid = create_list_with_numbers(map)
     matrix = make_matrix(grid)
     grid = solve(matrix, find_position(matrix))
     print(print_map(grid))
-    
