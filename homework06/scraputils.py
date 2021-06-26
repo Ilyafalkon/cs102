619b41a6e00858d42abb6baa319818f035189c7c
ilyafalkon <sokolov2001ilya@gmail.com>
2021-06-26 13:52:21 +0300

    Added tests

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,47 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-from db import News, session
-
-Base = declarative_base()
-engine = create_engine("sqlite:///news.db")
-session = sessionmaker(bind=engine)
 
 
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
-    info = {}
-    table = parser.find_all("td", "title")
-    table_2 = parser.find_all("td", "subtext")
-    signal_of_title = 1
-    for i in range(1, len(table), 2):
-        info["author"] = table_2[int(i / 2)].a.text
 
-        if table_2[int(i / 2)].find_all("a")[-1].text == "discuss":
-            info["comments"] = 0
-        else:
-            info["comments"] = int(table_2[int(i / 2)].find_all("a")[-1].text.split()[0])
-
-        info["points"] = int(table_2[int(i / 2)].span.text.split()[0])
-
-        info["title"] = table[i].a.text
-
-        info["url"] = table[i].a.get("href")
-
-        news_list.append(info)
-        info = {}
+    tr_list = parser.body.findAll('table')[2].findAll('tr')
+    for i,tr in enumerate(tr_list[:-2]):
+        if i%3 == 0:
+            news_list.append({})
+            news_list[-1]['title'] = tr.findAll('td')[-1].a.text
+            news_list[-1]['url'] = tr.findAll('td')[-1].a.get('href')
+        if i%3 == 1:
+            if len(tr.findAll('td')[1].findAll('span')) == 3:
+                news_list[-1]['author'] = tr.findAll('td')[1].findAll(
+                'a'
+                )[0].text
+                points_str = tr.findAll('td')[1].findAll('span')[0].text
+                news_list[-1]['points'] = int(points_str[:points_str.find('point')-1])
+                comments_str = tr.findAll('td')[1].findAll('a')[-1].text
+                if 'comment' in comments_str:
+                    news_list[-1]['comments'] = int(comments_str[:comments_str.find('comment')-1])
+                else:
+                    news_list[-1]['comments'] = 0
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    table = parser.find_all("td", "title")
-    next_page = table[-1].a.get("href")
-    return next_page
+    link_tag = parser.body.findAll('table')[2].findAll('tr')[-1].a
+    return link_tag.get('href')
+
 
 
 def get_news(url, n_pages=1):
@@ -58,6 +48,5 @@ def get_news(url, n_pages=1):
         n_pages -= 1
     return news
 
-
 if __name__ == "__main__":
-    get_news("https://news.ycombinator.com/newest")
+    get_news(url="https://news.ycombinator.com", n_pages=1)
---
+++ homework06/test_scraputils.py
@@ -0,0 +1,215 @@
+import pytest
+from bs4 import BeautifulSoup
+from scraputils import extract_news, get_news
+
+
+class TestExtractNews:
+    @pytest.fixture()
+    def html_with_news(self):
+        return """
+                <html lang="en" op="news">
+                <head>
+                    <title>Hacker News</title>
+                </head>
+                <body>
+                    <center>
+                        <table id="hnmain" border="0" cellpadding="0" cellspacing="0" width="85%" bgcolor="#f6f6ef">
+                            <tr>
+                                <td bgcolor="#ff6600">
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="padding:2px">
+                                        <tr>
+                                            here is title we don't need
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr id="pagespace" title="" style="height:10px"></tr>
+                            <tr>
+                                <td>
+                                    <table border="0" cellpadding="0" cellspacing="0" class="itemlist">
+                                        <tr class='athing' id='26803201'>
+                                            <td align="right" valign="top" class="title">
+                                                <span class="rank">1.</span>
+                                            </td>
+                                            <td valign="top" class="votelinks">
+                                                <center>
+                                                    <a id='up_26803201' href='vote?id=26803201&amp;how=up&amp;goto=news'>
+                                                        <div class='votearrow' title='upvote'></div>
+                                                    </a>
+                                                </center>
+                                            </td>
+                                            <td class="title">
+                                                <a href="https://y-n10.com/" class="storylink">Yamauchi No.10 Family Office</a>
+                                                <span class="sitebit comhead">
+                                                     (
+                                                    <a href="from?site=y-n10.com">
+                                                        
+                                                        {link}
+                                                        
+                                                    </a>
+                                                    )
+                                                </span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="2"></td>
+                                            <td class="subtext">
+                                            
+                                                {score_n_author}
+                                                
+                                                <span class="age">
+                                                    <a href="item?id=26803201">4 hours ago</a>
+                                                </span>
+                                                <span id="unv_26803201"></span>
+                                                 | 
+                                                <a href="hide?id=26803201&amp;goto=news">hide</a>
+                                                 | 
+                                                <a href="item?id=26803201">88&nbsp;comments</a>
+                                            </td>
+                                        </tr>
+                                        <tr class="spacer" style="height:5px"></tr>
+                                        <tr class="morespace" style="height:10px"></tr>
+                                        <tr>
+                                            <td colspan="2"></td>
+                                            <td class="title">
+                                                <a href="news?p=2" class="morelink" rel="next">More</a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>  
+                            <tr>
+                                <td>
+                                    <table width="100%" cellspacing="0" cellpadding="1">
+                                        <tr>
+                                            <td bgcolor="#ff6600"></td>
+                                        </tr>
+                                    </table>
+                                    <br>
+                                    <center>
+                                        and here are an elements we don't need
+                                    </center>
+                                </td>
+                            </tr>
+                        </table>
+                    </center>
+                </body>
+                </html>
+                """
+
+    @pytest.fixture()
+    def html_without_news(self):
+        return """
+                <html lang="en" op="news">
+                    <head>
+                        <title>Hacker News</title>
+                    </head>
+                    <body>
+                        <center>
+                            <table id="hnmain" border="0" cellpadding="0" cellspacing="0" width="85%" bgcolor="#f6f6ef">
+                                <tr>
+                                    <td bgcolor="#ff6600">
+                                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="padding:2px">
+                                            <tr>
+                                                title we don't need
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr id="pagespace" title="" style="height:10px"></tr>
+                                <tr>
+                                    <td>
+                                        <table border="0" cellpadding="0" cellspacing="0" class="itemlist">
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <table width="100%" cellspacing="0" cellpadding="1">
+                                            <tr>
+                                                <td bgcolor="#ff6600"></td>
+                                            </tr>
+                                        </table>
+                                        <br>
+                                        <center>
+                                            nothing we need
+                                        </center>
+                                    </td>
+                                </tr>
+                            </table>
+                        </center>
+                    </body>
+                </html>
+                """
+
+    def test_with_full_amount_of_data(self, html_with_news):
+        # fmt: off
+        html = html_with_news.format(link='<span class="sitestr">y-n10.com</span>', score_n_author='<span class="score" id="score_26803201">297 points</span> by <a href="user?id=cmod" class="hnuser">cmod</a>')
+        # fmt: on
+        page = BeautifulSoup(html, "html.parser")
+        news_list = extract_news(page)
+        expected_news_list = [
+            {
+                "title": "Yamauchi No.10 Family Office",
+                "short_url": "y-n10.com",
+                "long_url": "https://y-n10.com/",
+                "score": 297,
+                "author": "cmod",
+            }
+        ]
+
+        assert expected_news_list.sort() == news_list.sort()
+
+    def test_without_short_link(self, html_with_news):
+        # fmt: off
+        html = html_with_news.format(link='', score_n_author='<span class="score" id="score_26803201">297 points</span> by <a href="user?id=cmod" class="hnuser">cmod</a>')
+        # fmt: on
+        page = BeautifulSoup(html, "html.parser")
+        news_list = extract_news(page)
+        expected_news_list = [
+            {
+                "title": "Yamauchi No.10 Family Office",
+                "short_url": "news.ycombinator.com",
+                "long_url": "https://news.ycombinator.com/https://y-n10.com/",
+                "score": 297,
+                "author": "cmod",
+            }
+        ]
+
+        assert expected_news_list.sort() == news_list.sort()
+
+    def test_without_author(self, html_with_news):
+        # fmt: off
+        html = html_with_news.format(link='<span class="sitestr">y-n10.com</span>', score_n_author='<span class="score" id="score_26803201">297 points</span>')
+        # fmt: on
+        page = BeautifulSoup(html, "html.parser")
+        news_list = extract_news(page)
+        expected_news_list = [
+            {
+                "title": "Yamauchi No.10 Family Office",
+                "short_url": "y-n10.com",
+                "long_url": "https://y-n10.com/",
+                "score": 297,
+                "author": "-",
+            }
+        ]
+
+        assert expected_news_list.sort() == news_list.sort()
+
+    def test_without_score(self, html_with_news):
+        # fmt: off
+        html = html_with_news.format(link='<span class="sitestr">y-n10.com</span>', score_n_author='<a href="user?id=cmod" class="hnuser">cmod</a>')
+        # fmt: on
+        page = BeautifulSoup(html, "html.parser")
+        news_list = extract_news(page)
+        expected_news_list = [
+            {
+                "title": "Yamauchi No.10 Family Office",
+                "short_url": "y-n10.com",
+                "long_url": "https://y-n10.com/",
+                "score": 0,
+                "author": "cmod",
+            }
+        ]
+
+        assert expected_news_list.sort() == news_list.sort()
